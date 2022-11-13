8649c8a3d8e3139b4811680d09cc61df87d49199
Lewis Campbell <info@lewiscampbell.tech>
2022-11-13 17:53:32 +1300

    mend

---
+++ shim.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <wchar.h>
+#include "termbox2.h"
+
+void term_new() {
+	tb_init();
+	tb_set_output_mode(TB_OUTPUT_TRUECOLOR);
+}
+
+struct tb_event term_wait_for_event() {
+	struct tb_event ev;
+	tb_poll_event(&ev);
+	return ev;
+}
+
+void term_print(int x, int y, const char* str) {
+	tb_printf(x, y, 0xFFFFFF, 0x000000, str);
+}
